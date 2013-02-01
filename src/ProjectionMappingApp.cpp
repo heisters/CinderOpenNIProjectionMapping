24b8ea3d44fd56409147cc59bbaaedefb94cf1cb
Ian Smith-Heisters <i@idiosyncra.tc>
2013-01-31 21:14:27 -0800

    Haxxy pass at finding a chessboard and generating camera calibrations

--- src/ProjectionMappingApp.cpp
+++ src/ProjectionMappingApp.cpp
@@ -1,3 +1,12 @@
+/*
+ 
+ TODO:
+ - separate window for projector
+ - refactor
+ - only run calibration on demand
+ - record calibration observation when the grid is held still
+ */
+
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
 #include "cinder/gl/Vbo.h"
@@ -5,9 +14,12 @@
 #include "cinder/Camera.h"
 #include "cinder/params/Params.h"
 #include "cinder/CinderMath.h"
+#include "CinderOpenCv.h"
 
 #define _USE_MATH_DEFINES
 #include <math.h>
+
+#include <boost/foreach.hpp>
 
 #include "VKinect.h"
 #include "VFakeKinect.h"
@@ -33,12 +45,20 @@ public:
     void draw();
     
 private:
-    FakeKinect kinect;
+    void drawDebug();
+    
+    void captureChessboardObservation();
+    
+    Kinect kinect;
     
     ci::gl::VboMesh vbo;
     ci::gl::GlslProg shader;
     
     ci::CameraPersp camera;
+    
+    vector< Vec3f > cornerPoints;
+    vector< vector < cv::Point3f > > chessboardObservations;
+    vector< vector < cv::Point2f > > chessboardStates;
     
     
     class UI {
@@ -176,14 +196,25 @@ void ProjectionMappingApp::update()
             point = pointCloud + it.getIndex();
             it.setPosition(point->X * 0.5f, point->Y * 0.5f, -(point->Z * 0.5));
         }
+        
+        Surface8u surface = Surface8u(kinect.getColorImage());
+        cv::Mat currentFrame = toOcvRef(surface);
+        vector< cv::Point2f > corners;
+        corners.reserve(9 * 6);
+        cv::findChessboardCorners(currentFrame, cv::Size(9, 6), corners);
+        
+        cornerPoints.clear();
+        for (vector< cv::Point2f >::iterator it = corners.begin() ; it < corners.end(); it++ ) {
+            int index = kinect.getColorSize().x * (int)it->y + (int)it->x;
+            point = pointCloud + index;
+            cornerPoints.push_back(Vec3f(point->X * 0.5f, point->Y * 0.5f, -(point->Z * 0.5)));
+        }
     }
 }
 
 void ProjectionMappingApp::draw()
 {
     gl::clear( Color( 0, 0, 0 ) );
-    
-    ui.draw();
     
     gl::pushMatrices();
     gl::setMatrices(camera);
@@ -200,6 +231,48 @@ void ProjectionMappingApp::draw()
     }
     gl::popMatrices();
     
+    drawDebug();
+}
+
+void ProjectionMappingApp::drawDebug()
+{
+    ui.draw();
+
+    if ( ui().isVisible() ) {
+        gl::pushMatrices();
+        gl::setMatrices(camera);
+        {
+            drawCoordinateFrame(250.f, 3.f, 2.f);
+            
+            BOOST_FOREACH(Vec3f p, cornerPoints)
+            {
+                gl::color(1, 0, 0);
+                gl::drawSphere(p, 2);
+            }
+        }
+        gl::popMatrices();
+    }
+}
+
+void ProjectionMappingApp::captureChessboardObservation() {
+    if ( cornerPoints.size() != 9 * 6 ) {
+        console() << "NO CAPTCHA!!" << endl;
+        return;
+    }
+    
+    console() << "YUMMY!" << endl;
+    
+    vector< cv::Point3f > observation;
+    vector< cv::Point2f > state;
+    int i = 0;
+    BOOST_FOREACH(Vec3f p1, cornerPoints)
+    {
+        observation.push_back(cv::Point3f(p1.x, p1.y, p1.z));
+        state.push_back(cv::Point2f(i % 9, i / 9));
+        i++;
+    }
+    chessboardObservations.push_back(observation);
+    chessboardStates.push_back(state);
 }
 
 void ProjectionMappingApp::keyDown(KeyEvent ev)
@@ -209,6 +282,29 @@ void ProjectionMappingApp::keyDown(KeyEvent ev)
         ui.cameraDistance = 100.f;
     } else if ( ev.getCode() == KeyEvent::KEY_BACKSLASH ) {
         ui().isVisible() ? ui().hide() : ui().show();
+    } else if ( ev.getCode() == KeyEvent::KEY_RETURN ) {
+        captureChessboardObservation();
+    } else if ( ev.getCode() == KeyEvent::KEY_x ) {
+        cv::Mat cameraMatrix = cv::initCameraMatrix2D(chessboardObservations, chessboardStates, cv::Size(9, 6));
+        
+        cv::Mat distCoeffs;
+        vector< cv::Mat > rvecs, tvecs;
+        
+        cv::calibrateCamera(chessboardObservations, chessboardStates, cv::Size(9, 6), cameraMatrix, distCoeffs, rvecs, tvecs, CV_CALIB_USE_INTRINSIC_GUESS);
+
+        console() << cameraMatrix << endl;
+        BOOST_FOREACH(cv::Mat v, rvecs)
+        {
+            console() << v << endl;
+        }
+        BOOST_FOREACH(cv::Mat v, tvecs)
+        {
+            console() << v << endl;
+        }
+
+        
+        chessboardObservations.clear();
+        chessboardStates.clear();
     }
 }
 
